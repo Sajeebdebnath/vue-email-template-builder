c3291ffa47fcad50b6bdb60d3171b5680e5b4bf7
Sajeeb Debnath <sajeebdebnath.cse@gmail.com>
2024-03-17 16:51:37 +0600

    Email Template Builder

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div id="bar">
+        <h1>Startech Email Template Builder</h1>
+        <button v-on:click="exportHtml">Export HTML</button>
+      </div>
+
+      <EmailEditor
+        ref="emailEditor"
+        v-on:load="editorLoaded"
+        style="height: 1000px"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { EmailEditor } from "vue-email-editor";
+import { saveAs } from "file-saver";
+
+export default {
+  name: "app",
+  components: {
+    EmailEditor,
+  },
+  methods: {
+    editorLoaded() {
+      // Pass your template JSON here
+      // this.$refs.emailEditor.editor.loadDesign({});
+    },
+    exportHtml() {
+      this.$refs.emailEditor.editor.exportHtml((data) => {
+        console.log("exportHtml", data);
+        const blob = new Blob([data.html], { type: "text/html" });
+        saveAs(blob, "email_template.html");
+      });
+    },
+  },
+};
+</script>
